b540e76ed1d6276aa342fe689a4a1d78f19885be
nokotan <kamenokonokotan@gmail.com>
2022-09-07 15:09:03 +0900

    Update emscripten to 3.1.20

--- DownloadAssets.sh
+++ DownloadAssets.sh
@@ -8,8 +8,8 @@ function DownloadZip() {
     fi
 }
 
-DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/win/8c9e0a76ebed2c5e88a718d43e8b62452def3771/wasm-binaries.zip" zips/emscripten-3.1.8.zip
-DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/node-v14.15.5-win-x64.zip" zips/emscripten-node.zip
+DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/win/d92c8639f406582d70a5dde27855f74ecf602f45/wasm-binaries.zip" zips/emscripten-3.1.20.zip
+DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/node-v14.8.2-win-x64.zip" zips/emscripten-node.zip
 DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/python-3.9.2-1-embed-amd64+pywin32.zip" zips/emscripten-python.zip
 DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/portable_jre_8_update_152_64bit.zip" zips/emscripten-java.zip
 
@@ -20,7 +20,7 @@ function ExtractZip() {
     unzip ${ZipName} -d ${TargetFolder}
 }
 
-ExtractZip zips/emscripten-3.1.8.zip tmp/emscripten
+ExtractZip zips/emscripten-3.1.20.zip tmp/emscripten
 ExtractZip zips/emscripten-node.zip tmp/node
 ExtractZip zips/emscripten-python.zip tmp/python
 ExtractZip zips/emscripten-java.zip tmp/java
