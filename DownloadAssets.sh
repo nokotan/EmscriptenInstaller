166685c53027660c15f59586f68f9331dee88d95
nokotan <kamenokonokotan@gmail.com>
2022-09-07 15:18:06 +0900

    fix node version

--- DownloadAssets.sh
+++ DownloadAssets.sh
@@ -9,7 +9,7 @@ function DownloadZip() {
 }
 
 DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/win/d92c8639f406582d70a5dde27855f74ecf602f45/wasm-binaries.zip" zips/emscripten-3.1.20.zip
-DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/node-v14.8.2-win-x64.zip" zips/emscripten-node.zip
+DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/node-v14.18.2-win-x64.zip" zips/emscripten-node.zip
 DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/python-3.9.2-1-embed-amd64+pywin32.zip" zips/emscripten-python.zip
 DownloadZip "https://storage.googleapis.com/webassembly/emscripten-releases-builds/deps/portable_jre_8_update_152_64bit.zip" zips/emscripten-java.zip
 
